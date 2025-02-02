d3af8457f5b52c1d317ed81835992a212b8bf5cb
Ahmat-2000 <ahmatmhtlouky@gmail.com>
2025-02-02 22:47:54 +0100

    begin auth implementation

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,23 +1,25 @@
-"use server";
-import prisma from "@/app/lib/prismaClient";
-import bcrypt from "bcryptjs"; 
+// "use server";
+// import prisma from "@/app/lib/prismaClient";
+// import bcrypt from "bcryptjs"; 
 
-export async function loginUser(formData: FormData) {
-  const email = formData.get("email") as string;
-  const password = formData.get("password") as string;
+// export async function POST(formData: FormData) {
+export async function POST(request : Request) {
+//   const email = formData.get("email") as string;
+//   const password = formData.get("password") as string;
 
-  // Find user by email
-  const user = await prisma.user.findUnique({ where: { email } });
-  if (!user) {
-    return { error: "Invalid credentials" };
-  }
+//   // Find user by email
+//   const user = await prisma.user.findUnique({ where: { email } });
+//   if (!user) {
+//     return { error: "Invalid credentials" };
+//   }
 
-  // Compare passwords
-  const passwordMatch = await bcrypt.compare(password, user.password);
-  if (!passwordMatch) {
-    return { error: "Invalid credentials" };
-  }
+//   // Compare passwords
+//   const passwordMatch = await bcrypt.compare(password, user.password);
+//   if (!passwordMatch) {
+//     return { error: "Invalid credentials" };
+//   }
 
 
-  return { message: "Login successful" };
+//   return { message: "Login successful" };
+  return Response.json({});
 }
