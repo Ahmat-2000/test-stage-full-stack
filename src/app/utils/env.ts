01f2b4ad26a3c84968ef46493d1fa83e3614a986
Ahmat-2000 <ahmatmhtlouky@gmail.com>
2025-02-04 15:26:07 +0100

    Auth is complet now => next is favorites page

--- src/app/utils/env.ts
+++ src/app/utils/env.ts
@@ -2,7 +2,7 @@
 import {z} from "zod";
 
 const envSchema = z.object({
-  DATABASE_URL : z.string().url(),
+  DATABASE_URL : z.string(),
   // NEXT_PUBLIC_Twitch_API_ID : z.string(),
   // TWITCH_SECRET_API_KEY : z.string(),
   NEXT_PUBLIC_RAWG_API_KEY : z.string(),
