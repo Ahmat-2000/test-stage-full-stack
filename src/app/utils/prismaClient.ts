c183e1c2ace5646dc1f404a2f3f570fecfb1117b
Ahmat-2000 <ahmatmhtlouky@gmail.com>
2025-02-06 08:33:31 +0100

    update package.json for vercel

--- src/app/utils/prismaClient.ts
+++ src/app/utils/prismaClient.ts
@@ -2,8 +2,14 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-const prisma = globalForPrisma.prisma || new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "info", "warn", "error"] : ["warn", "error"],
+  });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
